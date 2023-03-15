24391ddc5f55b7a7dcf62f627223ac0dc11a7ac2
MeufKiBave <marieroy719@gmail.com>
2023-03-15 14:56:30 +0100

    Up 1

---
+++ Algo_KARAOKE_ROY.py
@@ -0,0 +1,49 @@
+import random
+
+class Player:
+    def __inti__ (self,name,score1,score2,score3,score4,score5,moyenscore):
+        self.__pseudo=name
+        self.__pscoresing1=score1
+        self.__pscoresing2=score2
+        self.__pscoresing3=score3
+        self.__pscoresing4=score4
+        self.__pscoresing5=score5
+        self.__moyenscoresing=moyenscore #1+2+3+4+5 diviser par 5
+
+    def gainscore(self,scoreenplus): #score minim 50 to 100
+        self.__
+
+#recup des infos player
+    def getpseudo(self):
+        return self.__pseudo
+
+    def getscore1(self):
+        return self.__pscoresing1
+
+    def getscore2(self):
+        return self.__pscoresing2
+
+    def getscore3(self):
+        return self.__pscoresing3
+
+    def getscore4(self):
+        return self.__pscoresing4
+
+    def getscore5(self):
+        return self.__pscoresing5
+
+    def getmoyen(self):
+        return self.__moyenscoresing
+
+class sing:
+    def __init__(self,titre1,titre2,titre3,titre4,titre5):
+        self.__1sing
+
+
+
+
+
+scooby= Player("Scooby",)
+samy= Player ("Samy", )
+fred= Player ("Fred", )
+vera= Player ("Véra", )
